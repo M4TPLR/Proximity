7b92a16a8a0925cb37ce56d1a5b06e44f729ca7d
Mathieu Peltier <mathieupeltier@rocketmail.com>
2018-08-02 23:00:11 +0200

    First migration
    The models and migrations: Adress, Category, Product, Store, User

--- database/migrations/2018_08_02_203759_create_stores_table.php
+++ database/migrations/2018_08_02_203759_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->mediumInteger('phone');
-            $table->string('role');
-            $table->string('imgurl');
-            $table->mediumText('bio');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('imgurl');
+            $table->string('ShippingType');
         });
     }
 
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 }
---
+++ database/migrations/2018_08_02_204133_create_adresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('firstLine');
+            $table->string('secondLine');
+            $table->string('country');
+            $table->string('city');
+            $table->integer('number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
